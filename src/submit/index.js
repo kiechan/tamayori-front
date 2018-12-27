ae3d11697d6226b3661080c63fa01b778646ee01
望月由登 <mochidzukiyutou@mochidzukiyutounoMacBook-puro.local>
2018-12-27 23:48:18 +0900

    登録画面を作成。

--- src/submit/index.js
+++ src/submit/index.js
@@ -42,25 +42,59 @@ const app = new Vue({
             app.ods.splice(index, 1)
         },
         setMunicipality: () => {
+            let prefecture = ''
+            for (let i = 0; i < app.select.prefectures.length; i++) {
+                if (app.odCommon.prefectures == app.select.prefectures[i].code) {
+                    prefecture = app.select.prefectures[i].name
+                    break
+                }
+            }
             $.ajax({
-                url: 'http://localhost:3000/v1/od/',
+                url: 'http://localhost:3000/municipality?prefecture=' + prefecture,
                 type: 'get'
-            }).done(() => {
-                app.raw = {}
-                app.od = {}
+            }).done((result) => {
+                console.log(result)
+                result.unshift({code: '', name: ''})
+                app.select.municipalities = result
             }).fail((error) => {
                 app.message = '市区町村を取得する処理でエラーが発生しました。時間を置いてからリトライしてください。'
                 console.log(error)
             })
         },
         add: () => {
+            let prefecture = ''
+            for (let i = 0; i < app.select.prefectures.length; i++) {
+                if (app.odCommon.prefectures == app.select.prefectures[i].code) {
+                    prefecture = app.select.prefectures[i].name
+                    break
+                }
+            }
+            let municipality = ''
+            for (let i = 0; i < app.select.municipalities.length; i++) {
+                if (app.odCommon.municipality == app.select.municipalities[i].code) {
+                    municipality = app.select.municipalities[i].name
+                    break
+                }
+            }
             $.ajax({
-                url: 'http://localhost:3000/v1/od/',
-                type: 'get',
-                data: app.od
+                url: 'http://localhost:3000/odsave/',
+                type: 'post',
+                data: {
+                    prefectures: prefecture,
+                    municipality: municipality,
+                    category_code: (app.odCommon.category != '') ? app.odCommon.category : null,
+                    category_name: app.odCommon.categoryElse,
+                    data: app.ods
+                }
             }).done(() => {
                 app.raw = {}
-                app.od = {}
+                app.ods = [{
+                    district: '',
+                    word: '',
+                    value: '',
+                    unit: '',
+                    url: ''
+                }]
             }).fail((error) => {
                 app.message = '追加処理でエラーが発生しました。時間を置いてからリトライしてください。'
                 console.log(error)
@@ -79,6 +113,17 @@ $(window).on('load', () => {
         app.select.prefectures = result
     }).fail((error) => {
         app.message = '県を取得する処理でエラーが発生しました。時間を置いてからリトライしてください。'
+        console.log(error)
+    })
+    $.ajax({
+        url: 'http://localhost:3000/category/',
+        type: 'get'
+    }).done((result) => {
+        console.log(result)
+        result.unshift({code: '', name: '（未選択）'})
+        app.select.categories = result
+    }).fail((error) => {
+        app.message = 'カテゴリを取得する処理でエラーが発生しました。時間を置いてからリトライしてください。'
         console.log(error)
     })
 })
